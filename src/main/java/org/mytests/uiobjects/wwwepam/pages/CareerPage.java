a7d7ebfbc3e6b8bad8ae41ecf569f029f2874b5d
PETERSBURG\Roman_Iovlev <romanyister@gmail.com>
2017-08-01 23:38:02 +0300

    fix test

--- src/main/java/org/mytests/uiobjects/wwwepam/pages/CareerPage.java
+++ src/main/java/org/mytests/uiobjects/wwwepam/pages/CareerPage.java
@@ -8,6 +8,6 @@ import org.openqa.selenium.support.FindBy;
  * Created by Roman_Iovlev on 10/22/2015.
  */
 public class CareerPage extends WebPage {
-    @FindBy(css = "job-search-input")
+    @FindBy(css = ".job-search-input")
     public ITextField keywords;
 }
