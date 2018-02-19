6c9e5cabd15c0c09fda9d1f33be932fbb81095f1
Supreme85 <roman_iovlev@epam.com>
2018-02-19 11:23:43 +0300

    minor page title change

--- src/main/java/org/mytests/uiobjects/example/site/pages/HomePage.java
+++ src/main/java/org/mytests/uiobjects/example/site/pages/HomePage.java
@@ -3,6 +3,6 @@ package org.mytests.uiobjects.example.site.pages;
 import com.epam.jdi.uitests.web.selenium.elements.composite.WebPage;
 import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.JPage;
 
-@JPage(url = "/index.html", title = "Index Page")
+@JPage(url = "/index.html", title = "Home Page")
 public class HomePage extends WebPage {
 }
